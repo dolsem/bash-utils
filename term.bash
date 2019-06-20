8602c050160a64bd0450bfdf94d93ce2d4c89575
Denis Semenenko <dols3m@gmail.com>
2019-06-20 10:38:33 -0400

    fix: make term.bash commands run without $TERM set

--- term.bash
+++ term.bash
@@ -12,12 +12,12 @@
 if [[ $DOLSEM_SHELL_COLLECTION_HELPERS_TERM == true ]]; then return; fi
 DOLSEM_SHELL_COLLECTION_HELPERS_TERM=true
 
-red() { tput setaf 1; cat; tput sgr0; }
-green() { tput setaf 2; cat; tput sgr0; }
-yellow() { tput setaf 3; cat; tput sgr0; }
-blue() { tput setaf 4; cat; tput sgr0; }
-magenta() { tput setaf 5; cat; tput sgr0; }
-cyan() { tput setaf 6; cat; tput sgr0; }
-white() { tput setaf 7; cat; tput sgr0; }
-dim() { tput dim; cat; tput sgr0; }
-reset_color() { tput sgr0; }
+red() { tput -T${TERM:-dumb} setaf 1; cat; tput -T${TERM:-dumb} sgr0; }
+green() { tput -T${TERM:-dumb} setaf 2; cat; tput -T${TERM:-dumb} sgr0; }
+yellow() { tput -T${TERM:-dumb} setaf 3; cat; tput -T${TERM:-dumb} sgr0; }
+blue() { tput -T${TERM:-dumb} setaf 4; cat; tput -T${TERM:-dumb} sgr0; }
+magenta() { tput -T${TERM:-dumb} setaf 5; cat; tput -T${TERM:-dumb} sgr0; }
+cyan() { tput -T${TERM:-dumb} setaf 6; cat; tput -T${TERM:-dumb} sgr0; }
+white() { tput -T${TERM:-dumb} setaf 7; cat; tput -T${TERM:-dumb} sgr0; }
+dim() { tput -T${TERM:-dumb} dim; cat; tput -T${TERM:-dumb} sgr0; }
+reset_color() { tput -T${TERM:-dumb} sgr0; }
